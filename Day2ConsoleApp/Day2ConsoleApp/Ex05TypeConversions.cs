95849362f4e31666e634dc11cdb5de958934f141
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2026-04-03 19:36:21 +0530

    Uploaded with Day 3 session

---
+++ Day2ConsoleApp/Day2ConsoleApp/Ex05TypeConversions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+//variable type conversions are the process of converting a value from one data type to another.
+//In C#, there are two types of type conversions: implicit and explicit.
+//Lower range variables can be implicitly set to higher range variables, but the reverse needs explicit conversion. This explicit conversions are called TYPE CASTING. Type casting is the process of converting a value from one data type to another by explicitly specifying the target type.
+//This can be done using the cast operator (()) or using the Convert class.
+//Ctrl+K+F is used to format the code in Visual Studio. It helps to improve the readability of the code by properly indenting and aligning the code elements.
+
+
+namespace Day2ConsoleApp
+{
+    internal class Ex05TypeConversions
+    {
+        static void Main(string[] args)
+        {
+            int iValue = 123; //shorter range variable
+            long dValue = iValue;//long is a higher range variable than int, so this conversion is implicit and does not require any special syntax.
+
+            //try the reverse of this:
+            int iVal2 = (int)dValue;//compile error, C# follows C Style casting..
+            Console.WriteLine(iVal2);
+
+            //What is the range of each data type?
+            Console.WriteLine("The Range of byte is " + byte.MinValue + " to " + byte.MaxValue);
+            Console.WriteLine($"The Range of Short is {short.MinValue} to {short.MaxValue}");//Interpolation syntax for string formatting, it is more readable and easier to use than concatenation.
+            Console.WriteLine($"The Range of integer is {int.MinValue} to {int.MaxValue}");
+            Console.WriteLine($"The Range of long is {long.MinValue} to {long.MaxValue}");
+            Console.WriteLine("The Range of Unsigned Long is {0} to {1}.The lowest value for unsigned is always 0", ulong.MinValue, ulong.MaxValue);//VS 2008 or something, VS2010 we have .NET 4.0, we can use interpolation syntax for string formatting, but in older versions of C#, we have to use the format specifier syntax for string formatting, which is less readable and more error-prone than interpolation syntax.
+
+            //TODO: Display the range of all the data types of C# language. 
+
+            /////////////////CONVERT CLASS//////////////////////
+            int iVal3 = Convert.ToInt32(123.45);//this will convert the double value 123.45 to an integer value 123 by truncating the decimal part.
+            Console.WriteLine($"The value of iVal3 is {iVal3}");
+            double dValue2 = Convert.ToDouble("123.456");//From which type to which type are we converting? string to double.
+            long lValue2 = Convert.ToInt64(123.45);//this will convert the double value 123.45 to a long value 123 by truncating the decimal part.
+            Console.WriteLine($"The Long value is {lValue2}" );
+            /////////////////////Why not CASTING?//////////////////////
+            long lValue3 = int.MaxValue;
+            int iVal = (int)(lValue3 + 123);//NOT SAFE, this will cause an overflow because the result of lValue3 + 123 exceeds the maximum value of int. The cast operator will not check for overflow and will simply truncate the result to fit into an int, which can lead to unexpected results.
+            //So it is always better to use the Convert class for type conversions, as it provides methods that can handle overflow and other conversion errors gracefully, by throwing exceptions that can be caught and handled in the code.
+            Console.WriteLine($"The Value of iVal is {iVal}");
+
+            iVal = Convert.ToInt32(lValue3 + 123);//throws an OverflowException instead of unexpected results.
+        }
+    }
+}
---
+++ Day2ConsoleApp/Day2ConsoleApp/Ex07BoxingAndUnBoxing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+//.NET gives a data type called object, which can be used to store any type of data. 
+//This type is the base type for all the data types of .NET. This is part of the CTS (Common Type System) of .NET. The object type can be used to store any type of data, but it is not recommended to use it for storing value types, as it can lead to performance issues due to boxing and unboxing.
+//When a value is assigned to an object, it hides its behaviour and stores the value as an object. This process is called boxing. 
+namespace Day2ConsoleApp
+{
+    internal class Ex07BoxingAndUnBoxing
+    {
+
+        //If U want to create a function that can return any type of data based on some condition that is evaluated at runtime, then the function's return type could be an object. To retrieve the original behavior of the value stored in the object, U need to perform unboxing, which is an explicit cast that U do for object types to retrieve back their original behavior.
+        //UNBOXING can be applied on only to that type from which it was boxed. If the original value was an int, then U can only unbox it to an int. If U try to unbox it to a different type, it will throw an InvalidCastException at runtime.
+        static void Main()
+        {
+            object value = 123; //U R storing an int into an object.
+            object value2 = 234.456;
+            object value3 = "Text Message";
+
+            object copy = (long)value + 123;//UNBOXING is an explicit cast that U do for object types to retrieve back their original behavior. 
+
+            //TODO: How is this different from var? 
+        }
+    }
+}
